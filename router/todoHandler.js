d5dc6e4f663fe6f12d842e74c033616c1706f5bd
jakaria98 <jhossain439@gmail.com>
2024-01-04 22:24:30 -0330

    router path added only

--- router/todoHandler.js
+++ router/todoHandler.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
 
+//get all todo
+router.get('/', async (req, res) => {});
+
+//get a todo by id
+router.get('/:id', async (req, res) => {});
+
+//post a todo
+router.post('/', async (req, res) => {});
+
+//post multiple todo
+router.post('/all', async (req, res) => {});
+
+//update a todo
+router.put('/:id', async (req, res) => {});
+
+//delete a todo
+router.delete('/:id', async (req, res) => {});
 module.exports = router;
