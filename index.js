484143e2bee76e56b6637f9e3774093aecd075ba
jakaria98 <jhossain439@gmail.com>
2024-01-04 22:03:45 -0330

    todo api created and server initialized

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const todoHandler = require('./schemas/todoSchema');
+// initialize app
+const app = express();
+app.use(express.json());
+
+// connect database
+mongoose
+    .connect('mongodb://localhost/todos')
+    .then(() => console.log('database connected'))
+    .catch((err) => console.log(err));
+
+// app routes
+app.use('/todo', todoHandler);
+
+// default error handler
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500).json({ error: err });
+}
+
+// start server
+app.listen(3000, () => {
+    console.log('server connected');
+});
